f445a42aeb5ddbf8b92022d40b566a6799747ccb
Sober Stilinski <soberstilinski@gmail.com>
2021-12-25 13:20:05 +0500

    FaceApi inserted and Face Detection Done

---
+++ fdb.js
@@ -0,0 +1,34 @@
+const video = document.getElementById('v1')
+
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models')
+]).then(startVideo)
+
+function startVideo() {
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(function (stream) {
+                video.srcObject = stream;
+            })
+            .catch(function (err0r) {
+                console.log("Something went wrong!");
+            });
+    }
+}
+
+video.addEventListener('playing', () => {
+
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.getElementById("video").append(canvas)
+    const displaySize = { width: video.width, height: video.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks()
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        faceapi.draw.drawDetections(canvas, resizedDetections)
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    }, 50)
+})
